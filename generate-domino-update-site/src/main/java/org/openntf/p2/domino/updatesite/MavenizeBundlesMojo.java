6255e99094b604f16a47cd0e26327e9792c09b15
Jesse Gallagher <jesse@secondfoundation.org>
2020-12-11 09:53:24 -0500

    Remove unused import

--- generate-domino-update-site/src/main/java/org/openntf/p2/domino/updatesite/MavenizeBundlesMojo.java
+++ generate-domino-update-site/src/main/java/org/openntf/p2/domino/updatesite/MavenizeBundlesMojo.java
@@ -43,7 +43,6 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
